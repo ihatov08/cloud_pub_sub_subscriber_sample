6fd7da3bff01df1ef451b97513da52317f36b2ce
ihatov08 <ihatov08@gmail.com>
2021-01-08 12:57:18 +0900

    requestのheaderをログに出力するように修正

--- app/controllers/api/v1/cloud_pub_sub/subscribe_controller.rb
+++ app/controllers/api/v1/cloud_pub_sub/subscribe_controller.rb
@@ -2,5 +2,6 @@ class Api::V1::CloudPubSub::SubscribeController < ApplicationController
   def create
     Rails.logger.info response.inspect
     Rails.logger.info params.inspect
+    Rails.logger.info request.headers.sort.map { |k, v| logger.info "#{k}:#{v}" }
   end
 end
