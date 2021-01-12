24e9e248ff2a870f18c26226719a68dadfc87077
ihatov08 <ihatov08@gmail.com>
2021-01-12 17:59:02 +0900

    subscriptionの状態と承認をするメソッドを追加

--- app/models/subscribe_log.rb
+++ app/models/subscribe_log.rb
@@ -1,2 +1,47 @@
 class SubscribeLog < ApplicationRecord
+  def get_purchase_subscription
+    service.get_purchase_subscription(package_name, subscription_id, purchase_token)
+  end
+
+  def acknowledge_purchase_subscription
+    service.acknowledge_purchase_subscription(package_name, subscription_id, purchase_token)
+  end
+
+  private
+
+  def decoded_message_data
+    if params
+      decoded = Base64.decode64 params.dig("message", "data")
+      JSON.parse(decoded)
+    end
+  end
+
+  def purchase_token
+    decoded_message_data&.dig("subscriptionNotification", "purchaseToken")
+  end
+
+  def package_name
+    decoded_message_data&.fetch("packageName")
+  end
+
+  def subscription_id
+    decoded_message_data&.dig("subscriptionNotification", "subscriptionId")
+  end
+
+  def scope
+    'https://www.googleapis.com/auth/androidpublisher'
+  end
+
+  def authorizer
+    Google::Auth::ServiceAccountCredentials.make_creds(scope: scope)
+  end
+
+  def service
+    @service ||=
+    begin
+      service = Androidpublisher::AndroidPublisherService.new
+      service.authorization = authorizer
+      service
+    end
+  end
 end
--- app/serializers/subscribe_log_serializer.rb
+++ app/serializers/subscribe_log_serializer.rb
@@ -1,9 +1,3 @@
 class SubscribeLogSerializer < ActiveModel::Serializer
   attributes :id, :token, :payload, :params, :decoded_message_data
-
-  def decoded_message_data
-    if object.params
-      Base64.decode64 object.params.dig("message", "data")
-    end
-  end
 end
